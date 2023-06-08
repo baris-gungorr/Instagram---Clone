e70ff94336d638f8dcf8cfad92eb7f02e9c31a64
Baris Gungor <gungorbaris41@gmail.com>
2023-06-08 17:32:15 +0300

    add permission

--- app/src/main/java/com/barisgungorr/UploadActivity.kt
+++ app/src/main/java/com/barisgungorr/UploadActivity.kt
@@ -1,25 +1,154 @@
 package com.barisgungorr
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
-import com.barisgungorr.instagramclone.R
-import com.barisgungorr.instagramclone.databinding.ActivityMainBinding
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 import com.barisgungorr.instagramclone.databinding.ActivityUploadBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
+import java.io.ByteArrayOutputStream
 
 class UploadActivity : AppCompatActivity() {
     private lateinit var binding: ActivityUploadBinding
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    var selectedPicture: Uri? = null
+    private var selectedBitmap: Bitmap? = null
+    private lateinit var auth : FirebaseAuth
+    private lateinit var firestore : FirebaseFirestore
+    private lateinit var storage : FirebaseStorage
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityUploadBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        registerLauncher()
 
+        auth = Firebase.auth
+        firestore = Firebase.firestore
+        storage = Firebase.storage
     }
+
+    private fun registerLauncher() {
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    val intentFromResult = result.data
+                    if (intentFromResult != null) {
+                        selectedPicture = intentFromResult.data
+                        selectedPicture?.let {
+                            binding.imageView.setImageURI(it)
+                            binding.imageView.scaleType = ImageView.ScaleType.CENTER_INSIDE
+
+                        }
+                    }
+                }
+            }
+
+        permissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+                if (result) {
+                    // permission granted
+                    val intentToGallery =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    activityResultLauncher.launch(intentToGallery)
+                } else {
+                    Toast.makeText(this@UploadActivity, "Permission needed!", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+    }
+
     fun upload(view: View) {
+        if (selectedBitmap != null) {
+            val smallBitmap = makeSmallerBitmap(selectedBitmap!!, 300)
+            val outputStream = ByteArrayOutputStream()
+            smallBitmap.compress(
+                Bitmap.CompressFormat.PNG,
+                50,
+                outputStream
+            )   //görseli byteDizisine çevirmek
+            val byteArray = outputStream.toByteArray()
+            // Diğer işlemleri buraya ekleyin
+        }
 
     }
-    fun selectimage(view:View) {
 
+    fun selectImage(view: View) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            ) {
+                Snackbar.make(view, "Permission needed for gallery", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Give permission") {
+                        //request permmision
+                        permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                    }.show()
+            } else {
+                //request permission
+                permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        } else {
+            val intentToGallery =
+                Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            //start activity for result
+            activityResultLauncher.launch(intentToGallery)
+        }
+    }
+
+    private fun makeSmallerBitmap(
+        image: Bitmap,
+        maximumSize: Int
+    ): Bitmap {
+        var width = image.width
+        var height = image.height
+
+        val bitmapRatio: Double = width.toDouble() / height.toDouble()
+
+        if (bitmapRatio > 1) {
+            // Landscape
+            width = maximumSize
+            val scaledHeight = (width.toDouble() / bitmapRatio).toInt()
+            height = scaledHeight.coerceAtMost(maximumSize)
+        } else if (bitmapRatio < 1) {
+            // Portrait
+            height = maximumSize
+            val scaledWidth = (height.toDouble() * bitmapRatio).toInt()
+            width = scaledWidth.coerceAtMost(maximumSize)
+        } else {
+            // Square
+            width = maximumSize
+            height = maximumSize
+        }
+
+        return Bitmap.createScaledBitmap(image, width, height, true)
     }
 }
+
